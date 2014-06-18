0e27e5c2a942ed935fda3865c7c0a94cfd347876
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2014-06-18 18:23:21 +0200

    Add Makefile to build external .jar files; cleanups

--- wikitoki-library/java/project.clj
+++ wikitoki-library/java/project.clj
@@ -5,3 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   ; :dependencies [[org.clojure/clojure "1.5.1"]]
   :java-source-paths ["src"])
+
+;; You have to run this after you update the jar file:
+; mvn install:install-file -Dfile=target/fake-android-context-0.1.0-SNAPSHOT.jar -DgroupId=local -DartifactId=fake-android-context -Dversion=0.1.0 -Dpackaging=jar
